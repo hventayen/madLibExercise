1933dadb4371b89429dd00d862c4aa36a4bc5c16
hventayen <harrold.ventayen@gmail.com>
2025-03-28 15:53:09 -0700

    starter code

---
+++ madLibs.js
@@ -0,0 +1,16 @@
+//madLibs.js
+/* Step 2: Declare and assign values to variables
+Include:
+Strings (e.g., const timeOfDay = "sunny afternoon";).
+Numbers (e.g., const mathResult = 2 + 2;).
+A boolean expression (e.g., const booleanValue = timeOfDay.includes("afternoon");).
+An object with properties (e.g., const person = { name: "Mike", age: 25 };).
+*/
+// Step 1: Paste in one of the story templates above
+const story = `
+// todo
+
+
+`;
+document.getElementById("madlibs-output").textContent = story;
+console.log(story);
